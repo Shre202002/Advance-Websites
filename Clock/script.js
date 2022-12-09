3953ad03077481f5cc1ec1960662daefb89de2bb
Shre <sriyanshgupta24@gmail.com>
2022-12-09 23:32:03 +0530

    Clock with Full Js HTML And CSS

--- Clock/script.js
+++ Clock/script.js
@@ -1,30 +1,31 @@
 function showTime() {
     let a = new Date();
-    let h = a.getHours;
-    let m = a.getMinutes;
-    let s = a.getSeconds;
-    let d = a.getDate;
-    let mon =a.getMonth
-    let year =a.getFullYear
+    let h = a.getHours();
+    let m = a.getMinutes();
+    let s = a.getSeconds();
+    let d = a.getDate();
+    let mon =a.getMonth();
+    let year =a.getFullYear();
     let season = "AM";
-
+console.log(h,m,s);
     if (h == 0) {
-        h == 12;
+        h = 12;
         season = "Am";
     }
     if (h > 12) {
         h = h - 12;
         season = "PM";
     }
-    // h = (h < 10) ? "0" + h : h;
-    // m = (m < 10) ? "0" + m : m;
-    // s = (s < 10) ? "0" + s : s;
-    let time = ` Time -${h}: ${m}: ${s} :${season} \n Date: ${d} / ${mon} / ${year}`;
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+
+    let time = ` Time: ${h}: ${m}: ${s} ${season} \nDate: ${d} / ${mon} / ${year}`;
 
 
-    document.querySelector("#Myclock").innerText = time;
-    document.querySelector("#Myclock").innerContent = time;
-    setTimeout(showTime(), 1000);
+    document.getElementById("Myclock").innerText = time;
+    document.getElementById("Myclock").innerContent = time;
+    setTimeout(showTime, 1000);
 }
 
 showTime()
